62ecf5ed8c34a480cbfc49baf1b370dbd3323f12
elenaferr0 <elena.ferro@gmail.com>
2024-03-29 14:57:48 +0100

    initialized docusaurus

---
+++ docs/intro.md
@@ -0,0 +1,21 @@
+---
+sidebar_position: 0
+---
+
+# Il gruppo
+
+## Componenti
+
+| Nome               | Matricola |
+|--------------------|-----------|
+| Leonardo Baldo     | 2042372   |
+| Antonio Benetazzo  | 2034528   |
+| Elena Ferro        | 2042328   |
+| Davide Malgarise   | 2009994   |
+| Valerio Occhinegro | 2011069   |
+| Raul Seganfreddo   | 1226293   |
+| Matteo Tiozzo      | 2042882   |
+
+## Contatti
+
+- Email: <a href="mailto:7last.swe@gmail.com">7last.swe@gmail.com</a>
---
+++ docusaurus.config.ts
@@ -0,0 +1,78 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+    title: '7Last',
+    tagline: 'Documentazione progetto SWE 2023/24',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://7last.github.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: '7Last', // Usually your GitHub org/user name.
+    projectName: 'docs', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'it',
+        locales: ['it'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts'
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        navbar: {
+            title: '7Last',
+            logo: {
+                alt: '7Last Logo',
+                src: 'img/logo.jpg',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'docsSidebar',
+                    position: 'left',
+                    label: 'Documentazione',
+                },
+                {
+                    href: 'https://github.com/7Last/docs',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            copyright: `Copyright © ${new Date().getFullYear()} - 7Last, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
