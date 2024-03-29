eab64018eb6d310ad14c501cb00956d2656d7a01
elenaferr0 <elena.ferro@gmail.com>
2024-03-29 22:45:38 +0100

    first release

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,14 @@ const config: Config = {
     url: 'https://7last.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/docusaurus',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: '7Last', // Usually your GitHub org/user name.
-    projectName: 'docs', // Usually your repo name.
+    projectName: 'docusaurus', // Usually your repo name.
+    deploymentBranch: 'gh-pages',
+    trailingSlash: false,
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
