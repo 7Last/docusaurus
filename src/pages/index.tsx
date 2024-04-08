acf3438274b9372c8aaed20cfbf760bb244a6287
elenaferr0 <elena.ferro@gmail.com>
2024-04-08 14:08:47 +0200

    chore: updated repo url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,21 @@ import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
-import {createTheme, ThemeProvider, Grid, Button} from "@mui/material";
-import {ArrowForward, GitHub} from "@mui/icons-material";
+import { createTheme, ThemeProvider, Grid, Button } from "@mui/material";
+import { ArrowForward, GitHub } from "@mui/icons-material";
 
 
 const theme = createTheme({
     components: {
         MuiButton: {
-            styleOverrides: {root: {lineHeight: 0}}
+            styleOverrides: { root: { lineHeight: 0 } }
         },
     },
 });
 
 
 function HomepageHeader() {
-    const {siteConfig} = useDocusaurusContext();
+    const { siteConfig } = useDocusaurusContext();
     return (
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
@@ -31,29 +31,29 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-    const {siteConfig} = useDocusaurusContext();
+    const { siteConfig } = useDocusaurusContext();
     return (
         <ThemeProvider theme={theme}>
             <Layout
                 title={siteConfig.title}>
-                <HomepageHeader/>
+                <HomepageHeader />
 
                 <Grid container spacing={5} p={5} justifyContent="center" alignItems="center"
-                      direction={{xs: 'column', md: 'row'}}>
-                    <Grid item xs={6} sx={{fontSize: '1.5em'}}>
+                    direction={{ xs: 'column', md: 'row' }}>
+                    <Grid item xs={6} sx={{ fontSize: '1.5em' }}>
                         Sito contenente la documentazione prodotta per il corso di Ingegneria del
                         Software, A.A. 2023/2024 presso l'Università degli Studi di Padova.
                     </Grid>
                     <Grid item xs={2}>
                         <Button variant="outlined" href={'/docs/intro'}
-                                sx={{backgroundColor: 'var(--ifm-color-primary)', color: '#000'}}>
+                            sx={{ backgroundColor: 'var(--ifm-color-primary)', color: '#000' }}>
                             <span>Introduzione</span>
-                            <ArrowForward/>
+                            <ArrowForward />
                         </Button>
                     </Grid>
 
                     <Grid container spacing={3} p={5} m={2} justifyContent={'center'} alignItems={'center'}
-                            direction={{xs: 'column', md: 'row'}}>
+                        direction={{ xs: 'column', md: 'row' }}>
                         <Grid item xs={4}>
                             <Heading as={'h2'}>Proof of Concept</Heading>
                             <p>
@@ -64,14 +64,14 @@ export default function Home(): JSX.Element {
 
                         <Grid item xs={2}>
                             <Button variant="outlined"
-                                    sx={{
-                                        color: 'var(--ifm-color-primary)',
-                                        border: '2px solid var(--ifm-color-primary)',
-                                        padding: 3,
-                                    }}
-                                    href={'https://github.com/7Last/PoC/'}
-                                    startIcon={<GitHub/>}
-                                    endIcon={<ArrowForward/>}>
+                                sx={{
+                                    color: 'var(--ifm-color-primary)',
+                                    border: '2px solid var(--ifm-color-primary)',
+                                    padding: 3,
+                                }}
+                                href={'https://github.com/7Last/SyncCity/'}
+                                startIcon={<GitHub />}
+                                endIcon={<ArrowForward />}>
                                 Repository
                             </Button>
                         </Grid>
